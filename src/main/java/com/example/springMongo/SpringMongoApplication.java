341345ea377de2145f10d380ef445198e4e4ddb8
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-12-03 02:19:58 +0000

    ...

--- src/main/java/com/example/springMongo/SpringMongoApplication.java
+++ src/main/java/com/example/springMongo/SpringMongoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringMongoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMongoApplication.class, args);
 	}
